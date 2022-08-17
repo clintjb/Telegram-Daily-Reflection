7b518efc871f394ed0179fa61e7cfb29284b9c8a
Clint Bird <clintjb@gmail.com>
2022-08-17 20:59:12 +0200

    Update script.py

--- script.py
+++ script.py
@@ -1,15 +1,15 @@
 # Import Libraries
 import requests
-import pandas as pd
+import random
 
 # Import Keys
 TELEGRAM_TOKEN = os.environ['TELEGRAM_TOKEN']
 SEND_ID = os.environ['SEND_ID']
 
-reflections = pd.read_csv('reflections.tsv', delimiter='\t', index_col=False, header=None)
+reflections = open('reflections2.tsv').read().splitlines()
 
 TOKEN = TELEGRAM_TOKEN
 CHAT_ID = SEND_ID
-message = reflections.sample()
+message = random.choice(lines)
 url = f"https://api.telegram.org/bot{TOKEN}/sendMessage?chat_id={CHAT_ID}&text={message}"
 print(requests.get(url).json()) # this sends the message
